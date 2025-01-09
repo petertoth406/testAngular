f9f63057ab0fed28ad48527b058f26b1b904ca20
Tóth Péter <petertoth406@gmail.com>
2025-01-09 12:02:31 +0100

    other

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
+import { OtherComponent } from './other/other.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
+    ChildComponent,
+    OtherComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/other/other.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-other',
+  standalone: false,
+  
+  templateUrl: './other.component.html',
+  styleUrl: './other.component.css'
+})
+export class OtherComponent {
+
+}
