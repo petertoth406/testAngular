79ceb380415c8d64fa3933786bcbe1d4c52e1d53
petertoth406 <petertoth406@gmail.com>
2025-01-30 16:38:30 +0100

    20250130

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+
+  id!: string;
+
+  constructor(
+    private route: ActivatedRoute
+  ){
+    this.id = route.snapshot.paramMap.get("id")!;
+  }
+
+  //Házi feladat:
+  /*
+  User serviceben metódust írni 1 db user lekérdezésére:
+  https://jsonplaceholder.typicode.com/users/{id} API-ról
+
+  HTML-en megjeleníteni a user nevét, és email címét
+  
+  */
+
 
 }
